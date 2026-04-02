a80e51f21c08107db4a9b19ad263203b865b342d
Felipe Neto Mello <fnm03br@gmail.com>
2026-04-02 20:58:30 -0300

    Adicionado Exercicio 05

--- Exercicio04.ConsoleApp/Program.cs
+++ Exercicio04.ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Crie um programa para calcular o salário total de um vendedor. 
+Deverá ser informado o salário base e o total de vendas. 
+A comissão é calculada com um percentual (informado pelo usuário) sobre o total de vendas.
+*/
+
+//Salário Total = Salário Fixo + (Total de Vendas * Porcentagem de Comissão)
+
+Console.Write("Digite o salário base do funcionário (em R$): ");
+double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\nDigite o total de vendas do funcionário (em R$): ");
+double totalVendas = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\nDigite a porcentagem de comissão do funcionário: ");
+double porcentagemVendas = Convert.ToDouble(Console.ReadLine());
+
+double salarioTotal = salarioBase + (totalVendas * (porcentagemVendas / 100));
+Console.WriteLine($"\nO salário total do funcionário será R$: {salarioTotal:F2}");
+
+Console.ReadLine();
