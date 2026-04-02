d37213daee02175fef2a19976b81d5cb0e18ecc0
Felipe Neto Mello <fnm03br@gmail.com>
2026-04-02 20:47:01 -0300

    Adicionado Exercicio 04

--- Exercicio03.ConsoleApp/Program.cs
+++ Exercicio03.ConsoleApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+//Fórmula ((0°C × 9)/5) + 32 = 32 °F
+
+Console.Write("Digite a temperatura em °C: ");
+double tempCelsius = Convert.ToDouble(Console.ReadLine());
+
+double tempFahrenheit = ((tempCelsius * 9) / 5) + 32;
+
+Console.WriteLine($"A temperatura de {tempCelsius} °C em Fahrenheit é: {tempFahrenheit} °F.");
+Console.ReadLine();
+
