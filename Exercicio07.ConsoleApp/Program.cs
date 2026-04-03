79948b3e8257815ef8378269c97b2b2c8cfc7c8a
Felipe Neto Mello <fnm03br@gmail.com>
2026-04-03 13:52:22 -0300

    Adicionado Exercicio 07

---
+++ Exercicio07.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+O IMC – Índice de Massa Corporal é um critério da Organização Mundial de Saúde para dar
+uma indicação sobre a condição de peso de uma pessoa adulta. A fórmula é IMC = peso /
+(altura) 2. Elabore um algoritmo que leia o peso e a altura de um adulto e mostre sua condição
+de acordo com a listagem abaixo:
+a. IMC em adultos Condição
+b. Abaixo de 18,5
+c. Abaixo do peso
+d. Entre 18,5 e 25 Peso normal
+e. Entre 25 e 30
+f. Acima do peso
+g. Acima de 30 obeso
+*/
